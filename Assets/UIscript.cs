70e39485eb3cfd3cf97b3a5b1eeb6539ac315d6c
TaigaIwashita <77519967+TaigaIwashita@users.noreply.github.com>
2021-03-05 11:05:58 +0900

    通信エラーメッセージの訂正

---
+++ Assets/UIscript.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class UIscript : MonoBehaviourPunCallbacks
+{
+
+    public GameObject nocon;
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        photonView.RPC(nameof(closepop), RpcTarget.All);
+        //base.OnLeftRoom();
+    }
+
+    [PunRPC]
+    void closepop()
+    {
+        nocon.gameObject.SetActive(true);
+    }
+}
